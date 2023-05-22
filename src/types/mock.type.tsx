51f8adfcc5d1526e11d25f50a51f0aa6af5656ec
Sofia Hudson <sofia-sama@hotmail.com>
2023-05-22 23:24:27 +0200

    Add react structure but not shows in the interface

---
+++ src/types/mock.type.tsx
@@ -0,0 +1,10 @@
+export type GentlemanData = {
+  id: number;
+  name: string;
+  status: string;
+  profession: string;
+  twitter: string;
+  picture: string;
+  alternativeText: string;
+  selected: boolean;
+};
